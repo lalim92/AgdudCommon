d446eb070beca68a8bfa51efcb5e087f2eb21637
Salim Abdous <salim.abdous@gmail.com>
2019-04-27 14:24:40 +0200

    first push

---
+++ Common/Models/Language.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Common.Models
+{
+    public sealed class Language
+    {
+        private string name;
+        private string cultureCode;
+        private CultureInfo cultureInfo;
+
+        public string Name { get => name; set => name = value; }
+        public string CultureCode { get => cultureCode; set => cultureCode = value; }
+        public CultureInfo CultureInfo { get => cultureInfo; set => cultureInfo = value; }
+
+        public Language()
+        {
+
+        }
+    }
+}
